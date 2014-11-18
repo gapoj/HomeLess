623552786091ec6f26012af75b65e09cc0ebfb9b
maxicasal <maximiliano.casal@globant.com>
2014-11-18 11:47:09 -0200

    details view

--- Source/Classes/DetailsHouseViewController.m
+++ Source/Classes/DetailsHouseViewController.m
@@ -22,7 +22,7 @@
     [scroller setScrollEnabled:YES];
     [scroller setContentSize:CGSizeMake(320, 1900)];
     if(!self.house.isDogAllowed){
-    self.dog.hidden=YES;
+        self.dog.hidden=YES;
     }
     if(!self.house.isCatAllowed){
         self.cat.hidden=YES;
@@ -32,69 +32,87 @@
     }
     self.desc.text = self.house.houseDescription;
     self.housetitle.text = self.house.title;
-   self.baths.text=[NSString stringWithFormat:@"%lu", self.house.bathrooms];
-  self.price.text=[NSString stringWithFormat:@"%lu", self.house.price];
-   self.squareMeters.text=[NSString stringWithFormat:@"%lu", self.house.bathrooms];
-   self.rooms.text=[NSString stringWithFormat:@"%lu", self.house.bathrooms];
+    self.baths.text=[NSString stringWithFormat:@"%lu", self.house.bathrooms];
+    self.price.text=[NSString stringWithFormat:@"%lu", self.house.price];
+    self.squareMeters.text=[NSString stringWithFormat:@"%lu", self.house.bathrooms];
+    self.rooms.text=[NSString stringWithFormat:@"%lu", self.house.bathrooms];
     
 }
+
 -(void)loadPhotos{
     PFQuery *photoQuery = [HousePhoto query];
     [photoQuery whereKey:@"house"equalTo:self.house];
     [photoQuery orderByDescending:@"isMain" ];
-   
+    
     [photoQuery findObjectsInBackgroundWithBlock:^(NSArray *photos, NSError *error) {
         if(error){
             NSLog(@"Error: %@",error);
         }else{
-           [self photosFromData:photos];
+            [self photosFromData:photos];
             self.pageControl.numberOfPages = [photos count];
             
         }
     }];
 }
+
 - (IBAction)onContact:(id)sender {
     SendMessageViewController *vc = [[SendMessageViewController alloc] init];
     vc.relatedHouse = self.house;
     vc.previousMessage = nil;
     [self showViewController:vc sender:self];
-
 }
+
 -(void)photosFromData:(NSArray * )array{
     NSMutableArray * loadPhotos =[[NSMutableArray alloc ]initWithCapacity:array.count];
     for (HousePhoto *photo in array) {
-      
+        
         PFFile *file =  photo.parsePhoto;
         if (file != nil) {
             NSError *error;
             NSData * data = [file getData: &error];
             if (!error) {
-               UIImage *image = [UIImage imageWithData:data];
+                UIImage *image = [UIImage imageWithData:data];
                 [loadPhotos addObject:image];
-               
+                
             }else{
                 NSLog(@"%@",error);
             }
-
-            }
+        }
     }
     self.image.image= loadPhotos[0];
     self.photos = loadPhotos;
 }
+
 - (IBAction)onEdit:(id)sender {
     AddHouseViewController *vc = [[AddHouseViewController alloc] init];
     vc.house =self.house;
     [self showViewController:vc sender:self];
 }
+- (IBAction)onNextPhotoPressed:(id)sender {
+    NSInteger pageIndex = self.pageControl.currentPage;
+    if (pageIndex < self.photos.count -1) {
+        pageIndex++;
+        self.image.image = self.photos[pageIndex];
+    }
+}
+
+- (IBAction)onPreviousPressed:(id)sender {
+    NSInteger pageIndex = self.pageControl.currentPage;
+    if (pageIndex >=1) {
+        pageIndex--;
+        self.image.image = self.photos[pageIndex];
+    }
+}
+
 - (IBAction)onPageChanged:(UIPageControl *)sender {
-    NSInteger a = sender.currentPage;
-    self.image.image = self.photos[a];
+    NSInteger pageIndex = sender.currentPage;
+    self.image.image = self.photos[pageIndex];
 }
 
 - (BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
-  
     return NO;
 }
+
 - (IBAction)onBack:(id)sender {
     [self dismissViewControllerAnimated:YES completion:nil];
 }
